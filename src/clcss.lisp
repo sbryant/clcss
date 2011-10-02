664c96c4915705c8eacb1e89f8673e402763c3bf
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-01 23:44:05 -0400

    [basic-garbage] Butchered the test into ugly and built a base reader loop that reads symbols. Aint that exciting.

--- src/clcss.lisp
+++ src/clcss.lisp
@@ -1,1 +1,7 @@
 (in-package :clcss)
+
+(defun read-css (path &key (start 0) acc)
+  (multiple-value-bind (data offset) (read-from-string path nil nil :start start)
+    (cond
+      ((null data) acc)
+      (t (read-css path :start offset :acc (reverse (cons data acc)))))))
