dbd1164382272ef0c1b6791740b64bb230487985
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-02 00:27:35 -0400

    [read-ids-better] Preserve whitespace in the read.

--- src/clcss.lisp
+++ src/clcss.lisp
@@ -7,7 +7,7 @@
 (defun read-css (path &key (start 0) acc)
   (let ((*readtable* (copy-readtable nil)))
     (set-macro-character #\# #'css-id-macro-character t)
-    (multiple-value-bind (data offset) (read-from-string path nil nil :start start)
+    (multiple-value-bind (data offset) (read-from-string path nil nil :start start :preserve-whitespace t)
       (cond
         ((null data) (reverse acc))
         (t (read-css path :start offset :acc (cons data acc)))))))
