61551c39783b0d2c5427aaa8fde5392a87c95814
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-02 05:32:07 -0400

    [stats] A make target to print the project LOC

---
+++ src/make/99-stat.mk
@@ -0,0 +1,17 @@
+#-*- mode:makefile-gmake; -*-
+locs:
+	@$(MAKE) locs-src
+	@$(MAKE) locs-tests
+	@$(MAKE) locs-total
+
+locs-src:
+	@echo "=> Source lines"
+	@find $(ROOT)/src -name '*.lisp' | xargs wc -l | grep total
+
+locs-tests:
+	@echo "=> Tests lines"
+	@find $(ROOT)/tests -name '*.lisp' | xargs wc -l | grep total
+
+locs-total:
+	@echo "=> All the locs"
+	@find $(ROOT)/tests $(ROOT)/src -name '*.lisp' | xargs wc -l | grep total
