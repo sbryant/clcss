7cce42c134d1e015041323e24583e4c580ec59c9
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-02 01:40:09 -0400

    [two-problems] Doing inital pre-processing with a set of regular expressions. We are now at two problems.

--- src/clcss.lisp
+++ src/clcss.lisp
@@ -4,10 +4,19 @@
   (declare (ignorable char))
   `(:id ,(read stream)))
 
-(defun read-css (path &key (start 0) acc)
+(defun read-css% (path &key (start 0) acc)
   (let ((*readtable* (copy-readtable nil)))
     (set-macro-character #\# #'css-id-macro-character t)
     (multiple-value-bind (data offset) (read-from-string path nil nil :start start :preserve-whitespace t)
       (cond
         ((null data) (reverse acc))
-        (t (read-css path :start offset :acc (cons data acc)))))))
+        (t (read-css% path :start offset :acc (cons data acc)))))))
+
+(defun transform-css-path (path)
+  (ppcre:regex-replace-all "(\\([^\\(]+?\\)[^\\w_-\\s]\\([^\\(]+?\\))+"
+                           (ppcre:regex-replace-all "([\\w_-]+)" path "(:word \\1)")
+                           "(\\1)"))
+
+
+(defun read-css (path)
+  (read-css% (transform-css-path path)))
