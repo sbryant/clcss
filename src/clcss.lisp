702692ff65f9740ba342d334d256a8b4968893c1
Sean Bryant <sean@vitrue.com>
2011-10-03 15:37:40 -0400

    [compiler] add a token emitter to emit tokens on event change.

--- src/clcss.lisp
+++ src/clcss.lisp
@@ -2,7 +2,9 @@
 
 (defclass fsm (c2mop:funcallable-standard-object)
   ((state :initarg :state :accessor state :initform nil)
-   (token-list :initarg :token-list :accessor token-list :initform nil))
+   (token-list :initarg :token-list :accessor token-list :initform nil)
+   (current-token :initform nil :accessor current-token)
+   (prefix :initform nil :accessor token-prefix))
   (:metaclass c2mop:funcallable-standard-class))
 
 (defmethod initialize-instance :before ((fsm fsm) &key)
@@ -22,21 +24,39 @@
 
 (defmethod read-symbol ((fsm fsm) event)
   (cond 
-    ((null event) nil)
+    ((null event)
+     'emit-token)
     ((ppcre:scan "[\\w-]" (string event))
+     (append-to-token fsm event)
      'read-symbol)
     ((equal event #\Space)
-     'read-space)
+     (emit-token fsm event 'read-space))
     ((equal event #\.)
-     'read-class)
+     (emit-token fsm event 'read-class))
     ((equal event #\#)
-     'read-id)))
-  
+     (emit-token fsm event 'read-id))))
+
+(defmethod emit-token ((fsm fsm) event &optional (next-event 'stop))
+  (let ((prefix (cond 
+                  ((equal 'read-id (state fsm)) :id)
+                  ((equal 'read-class (state fsm)) :class)
+                  (t :word))))
+    (format t "Emitting token: ~A~%" (current-token fsm))
+    (setf (token-list fsm) (append (token-list fsm) (list (list prefix (current-token fsm)))))
+    (setf (current-token fsm) nil)
+    (setf (token-prefix fsm) nil)
+    next-event))
+
+(defmethod append-to-token ((fsm fsm) c)
+  (setf (current-token fsm) (append (current-token fsm) (list c))))
+
 (defmethod read-class ((fsm fsm) event)
   (cond 
+    ((null event) (read-symbol fsm event))
     ((ppcre:scan "[\\w-]" (string event))
+     (append-to-token fsm event)
      'read-class)
-    (t 'read-symbol)))
+    (t (read-symbol fsm event))))
   
 (defmethod read-space ((fsm fsm) event)
   (if (equal event #\Space)
@@ -46,8 +66,9 @@
 (defmethod read-id ((fsm fsm) event)
   (cond 
     ((ppcre:scan "[\\w-]" (string event))
+     (append-to-token fsm event)
      'read-id)
-    (t 'read-symbol)))
+    (t (read-symbol fsm event))))
 
 (defun path-to-tokens (path)
   (tokenize-stream (make-string-input-stream path)))
@@ -55,9 +76,5 @@
 (defun tokenize-stream (stream)
   (do ((c (read-char stream nil nil) (read-char stream nil nil))
        (fsm (make-token-fsm) (funcall fsm c)))
-      ((or (null c) (null fsm)) fsm)
-    (format t "c: ~A fsm: ~A~%" c fsm)))
-    
-    
-
-              
+      ((or (null fsm) (equal (state fsm) 'stop)) fsm)
+    (format t "c: ~A fsm: ~A token-list: ~A~%" c fsm (token-list fsm))))
