86bd640b5e539fd4c68f7fe2d2cdd4ccf888ee70
Sean Bryant <sean@vitrue.com>
2011-10-03 01:08:12 -0400

    [compiler] WIP beginnings of a tokenizing state machine

--- src/clcss.lisp
+++ src/clcss.lisp
@@ -8,6 +8,7 @@
   (declare (ignorable char))
   `(:id ,(read stream)))
 
+
 (defun read-css% (path &key (start 0) acc)
   (let ((*readtable* (copy-readtable nil))
         (*package* (find-package :keyword)))
@@ -16,30 +17,52 @@
     (multiple-value-bind (data offset) (read-from-string path nil nil :start start :preserve-whitespace t)
       (cond
         ((null data) (reverse acc))
-        (t (read-css% path :start offset :acc (cons data acc)))))))
-
-(defun transform-css-path (path)
-  (flet ((paren-syms (s)
-           "Replace all symbols as CSS sees them with (:word symbol) to separate compund statements"
-           (ppcre:regex-replace-all "([\\w_-]+)" s
-                                    "(:word \\1)"))
-
-         (group-compound (s)
-           "Wrap groups of lists joined by a non-space into lists. e.g.:
-             (:word p)#(:word my-p) => (:compound (:word p)#(:word my-p))"
-           (ppcre:regex-replace-all "(\\([^\\(]+?\\)[^\\w_\\s-]\\([^\\(]+?\\))+" s
-                                    "(:compound \\1)"))
-
-         (dot-to-bang (s)
-           "Convert . to ! for lazy parsing"
-           (ppcre:regex-replace-all "\\." s
-                                    "!")))
-
-    (reduce #'(lambda (res proc) (funcall proc res))
-            (list #'dot-to-bang #'paren-syms #'group-compound)
-            :initial-value path)))
-
+        (t (read-css% path :start offset))))))
 
 (defun read-css (path)
-  (read-css% (transform-css-path path)))
+  (read-css% path))
 
+(defclass fsm (c2mop:funcallable-standard-object)
+  ((state :initarg :state :accessor state))
+  (:metaclass c2mop:funcallable-standard-class))
+
+(defmethod initialize-instance :before ((fsm fsm) &key)
+  (c2mop:set-funcallable-instance-function 
+   fsm 
+   #'(lambda (event)
+       (format t "state: ~A~%" (state fsm))
+       (format t "event: ~A~%" event)
+       (setf (state fsm) (funcall (state fsm) fsm event))
+       fsm)))
+       
+(defun make-token-fsm ()
+  (make-instance 'fsm :state 'read-symbol))
+
+(defmethod print-object ((fsm fsm) s)
+  (format s "#<FSM state: ~A>" (state fsm)))
+
+(defmethod read-symbol ((fsm fsm) event)
+  (cond 
+    ((null event) nil)
+    ((ppcre:scan "[\\w-]" (string event))
+     'read-symbol)
+    ((equal event #\Space)
+     'read-space)))
+
+(defmethod read-space ((fsm fsm) event)
+  (if (equal event #\Space)
+      'read-space
+      (read-symbol fsm event)))
+
+(defun path-to-tokens (path)
+  (tokenize-stream (make-string-input-stream path)))
+
+(defun tokenize-stream (stream)
+  (do ((c (read-char stream nil nil) (read-char stream nil nil))
+       (fsm (make-token-fsm) (funcall fsm c)))
+      ((or (null c) (null fsm)) fsm)
+    (format t "c: ~A fsm: ~A~%" c fsm)))
+    
+    
+
+              
