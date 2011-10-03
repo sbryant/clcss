0ee0021fa992ae0a167812370a54426ecd9e159c
Sean Bryant <sean@vitrue.com>
2011-10-03 10:33:55 -0400

    [compiler] remove read-css% symbol walker, not needed anymore.

--- src/clcss.lisp
+++ src/clcss.lisp
@@ -1,24 +1,5 @@
 (in-package :clcss)
 
-(defun css-class-macro-character (stream char)
-  (declare (ignorable char))
-  `(:class ,(read stream)))
-
-(defun css-id-macro-character (stream char)
-  (declare (ignorable char))
-  `(:id ,(read stream)))
-
-
-(defun read-css% (path &key (start 0) acc)
-  (let ((*readtable* (copy-readtable nil))
-        (*package* (find-package :keyword)))
-    (set-macro-character #\# #'css-id-macro-character t)
-    (set-macro-character #\! #'css-class-macro-character t)
-    (multiple-value-bind (data offset) (read-from-string path nil nil :start start :preserve-whitespace t)
-      (cond
-        ((null data) (reverse acc))
-        (t (read-css% path :start offset))))))
-
 (defun read-css (path)
   (read-css% path))
 
